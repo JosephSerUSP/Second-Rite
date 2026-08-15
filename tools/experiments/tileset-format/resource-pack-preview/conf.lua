8b0285fda0e8b5941a9d16ff66c4c4b61ff21ca5
Jo Seraph <josephseraph@usp.br>
2026-08-15 10:49:56 -0300

    Tilesets: add mixed-family packer LOVE config

---
+++ tools/experiments/tileset-format/resource-pack-preview/conf.lua
@@ -0,0 +1,9 @@
+function love.conf(t)
+    t.identity = "tileset-format-resource-pack"
+    t.console = true
+    t.window.title = "Thestra Surface palette runtime pack experiment"
+    t.window.width = 64
+    t.window.height = 64
+    t.window.visible = false
+    t.window.vsync = 0
+end

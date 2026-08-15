42568ef2fb6bdddaf9df056299737ac9d6816c58
Jo Seraph <josephseraph@usp.br>
2026-08-15 10:21:38 -0300

    Tilesets: add structural specimen LOVE config

---
+++ tools/experiments/tileset-format/structural-preview/conf.lua
@@ -0,0 +1,10 @@
+function love.conf(t)
+    t.identity = "tileset-format-structure"
+    t.console = true
+    t.window.title = "Thestra structural-profile experiment"
+    t.window.width = 768
+    t.window.height = 420
+    t.window.resizable = false
+    t.window.vsync = 0
+    t.window.depth = 24
+end

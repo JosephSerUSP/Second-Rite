18b3089f94696f0907ff7d41a1c5f9dc60ce7727
Jo Seraph <josephseraph@usp.br>
2026-08-18 17:34:36 -0300

    spike: run LÖVE parity without graphics or audio

---
+++ tools/experiments/shared-semantics/harness/love-conf.lua
@@ -0,0 +1,10 @@
+function love.conf(t)
+    t.window = false
+    t.modules.window = false
+    t.modules.graphics = false
+    t.modules.audio = false
+    t.modules.sound = false
+    t.modules.joystick = false
+    t.modules.physics = false
+    t.modules.video = false
+end

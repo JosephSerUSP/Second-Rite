5f4feec98df758ba254d9c061c6de8fd76f5cea2
Jo Seraph <josephseraph@usp.br>
2026-08-18 19:57:15 -0300

    test(#772): keep semantics harness headless

---
+++ tools/shared-semantics/love-conf.lua
@@ -0,0 +1,12 @@
+function love.conf(t)
+    t.identity = "thestra-shared-semantics-conformance"
+    t.console = true
+    t.window = nil
+    t.modules.audio = false
+    t.modules.graphics = false
+    t.modules.image = false
+    t.modules.joystick = false
+    t.modules.physics = false
+    t.modules.sound = false
+    t.modules.video = false
+end
